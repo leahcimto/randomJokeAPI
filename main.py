23d7e392420f109c12d7cc04bd399c88da2bf7b7
leahcimto <68165163+leahcimto@users.noreply.github.com>
2022-04-27 16:22:00 -0400

    resolves #1 #2

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import random
 app = Flask(__name__)
 
 # list of jokes
-with open('jokes.json') as file:
-  jokes = load(file)
+with open('jokes.txt') as file:
+  jokes = file.read().split("\n")
 
 @app.route('/')
 
 def home():
-  return  {"joke":jokes[random.randrange(0, len(jokes))]}
+  return  {"joke":jokes[random.choice(jokes)]}
 
 def run():
   app.run(host='0.0.0.0',port=8080)
