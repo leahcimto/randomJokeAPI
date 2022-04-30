db25f64a2c60c18573890fcbc5640e26ea9e017f
leahcimto <68165163+leahcimto@users.noreply.github.com>
2022-04-30 15:48:48 +0000

    revert 23d7e39

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from flask import Flask
 from threading import Thread
+from json import load
 import random
 
 app = Flask(__name__)
 
 # list of jokes
-with open('jokes.txt') as file:
-  jokes = file.read().split("\n")
+with open('jokes.json') as file:
+  jokes = load(file)
 
 @app.route('/')
 
 def home():
-  return  {"joke":jokes[random.choice(jokes)]}
+  return  {"joke":jokes[random.randrange(0, len(jokes))]}
 
 def run():
   app.run(host='0.0.0.0',port=8080)
